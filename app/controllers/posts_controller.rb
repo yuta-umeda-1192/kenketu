c6699aefce35014c38c39c13a0edc630a6ad11bb
baiden-house <57950871+baiden-house@users.noreply.github.com>
2020-02-05 17:05:49 +0900

    frist commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def index
+    # @posts = Post.all
+  end 
+
+#   def new
+#   end
+
+#   def create
+#     Post.create(post_params)
+#   end
+
+#   def destroy
+#     Post.destroy(post_params)
+#   end
+
+#   private
+#   def post_params
+#     params.permit(:content)
+#   end
+end
